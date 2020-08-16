06f6268079345e8dfdb4baa22884576688bf12cf
Kishen Makadia <kishenmakadia89@gmail.com>
2020-08-16 14:40:12 +0530

    Added admin seeder.

---
+++ database/seeds/AdminSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'first_name' => 'Admin',
+            'last_name' => 'Admin',
+            'email' => 'admin@example.com',
+            'phone' => '9999999999',
+            'password' => Hash::make('12345678'),
+            'type' => 'admin'
+        ]);
+    }
+}
