bdf506f4fea4c20ca4aa6fa1c55e83048d393597
Kishen Makadia <kishenmakadia89@gmail.com>
2020-08-16 14:42:03 +0530

    Fix : Phone column drop in users table.

--- database/migrations/2020_08_15_222656_phone_unique_for_users.php
+++ database/migrations/2020_08_15_222656_phone_unique_for_users.php
@@ -26,7 +26,7 @@ class PhoneUniqueForUsers extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropUnique('phone');
+            $table->dropColumn('phone');
         });
     }
 }
